219bf17a248fba62edb178695b250490195cd5b2
Casimiro Choia <csmrchoia@gmail.com>
2024-10-01 02:10:48 +0100

    'projecto_finalizado'

--- src/Functions.js
+++ src/Functions.js
@@ -1,4 +1,5 @@
 import chalk from "chalk";
+import md5 from 'md5';
 
 
 
@@ -7,13 +8,35 @@ export const bad = (string) => chalk.red(string)
 export const question = (string) => chalk.yellow(string)
 export const note = (string) => chalk.blue(string)
 export const end = () => "\n\t"
-export const URLLOCAL =(link) => {
+export const URLLOCAL = (link) => {
     if (link.includes("localhost")) return note('URL Local');
+    return ''
 }
 
 
 export const autocompleter = (line) => {
-    const words = ['https://','.com', new RegExp('https:\/\/[a-z0-9]{1,}\.com','i')]
+    const words = ['https://', '.com', 'http://localhost:5000/users']
     const hits = words.filter(word => word.startsWith(line))
     return [hits.length ? hits : words, line]
+}
+export const getDate = (date = new Date()) => {
+    let year = date.getFullYear();
+    let month_of_year = date.getMonth() + 1;
+    let day_of_month = date.getDate();
+
+    return `${day_of_month}${month_of_year}${year}`;
+}
+export const getHour = (date = new Date()) => {
+    let hour = date.getHours();
+    hour = hour < 10 ? '0' + hour : hour;
+    let min = date.getMinutes();
+    min = min < 10 ? '0' + min : min;
+    let sec = date.getSeconds();
+    sec = sec < 10 ? '0' + sec : sec;
+
+    return `${hour}${min}${sec}`;
+}
+
+export const generateHash = (private_key, public_key, ts = (getDate() + getHour())) => {
+    return md5(ts + private_key + public_key);
 }
